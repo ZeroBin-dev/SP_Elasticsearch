db9b2f8e7d0c56a12ecf4fcbd7aeac0b3170de29
CYB <zero-bin@naver.com>
2024-05-21 15:47:26 +0900

    init

---
+++ src/test/java/com/cyb/es/myshop/MyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyb.es.myshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyShopApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
